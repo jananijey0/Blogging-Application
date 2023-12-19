ea2f0283d89e108d0da4524f5badf7893d438e5c
jananijey0 <jananijey1@gmail.com>
2023-12-19 18:06:55 +0530

    Completed Editor Section

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,31 +1,96 @@
-import React, { useRef } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../imgs/logo.png'
 import AnimationWrapper from '../common/page-animation'
 import defaultBanner from '../imgs/blog banner.png'
 import { uploadImage } from '../common/aws'
+import { EditorContext } from '../pages/editor.pages'
+import {Toaster ,toast} from 'react-hot-toast'
+import EditorJS from '@editorjs/editorjs'
+import {tools} from './tools.component'
 const BlogEditor = () => {
-let blogBannerRef = useRef();
+// let blogBannerRef = useRef();
+let {blog,blog:{title,banner,content,tags,des},setBlog,textEditor,setTextEditor,setEditorState} = useContext(EditorContext)
+//useEffect 
+useEffect(()=>{
+   setTextEditor(new EditorJS({
+
+        holderId: 'textEditor',
+        data:'',
+        tools : tools,
+        placeholder:"Let's Write an Awesome story",
+    }))
+},[])
     const handleBannerUpload =(e) =>{
       
         let img = e.target.files[0];
  if(img){
+    let loadingToast = toast.loading("Uploading...")
+
     uploadImage(img).then((url) => {
+        
         if(url){
-  blogBannerRef.current.src = url 
+            toast.dismiss(loadingToast)
+            toast.success("Uploaded")
+//   blogBannerRef.current.src = url 
+  setBlog({...blog, banner:url})
         }
+    }) .catch(err =>{
+        toast.dismiss(loadingToast)
+        return toast.error(err);
     })
  }
+    }
+    const handleTitlekeyDown =(e)=>{
+
+if(e.keyCode ==13){//enter key code
+e.preventDefualt();
+}
+    }
+    const handleTitleChange =(e)=>{
+        let input = e.target;
+        input.style.height = 'auto';
+        input.style.height = input.scrollHeight + "px";
+        setBlog({...blog,title: input.value})
+    }
+    const handleError =(e)=> {
+        let img = e.target;
+        img.src = defaultBanner;
+    }
+
+    const handlePublishEvent =() => {
+        if(!banner.length){
+            return toast.error("Upload a Blog Banner to publish it")
+        }
+        if(!title.length){
+            return toast.error("Write a Title to publish the Blog")
+        }
+        if(textEditor.isReady){
+            textEditor.save().then(data => {
+                if(data.blocks.length){
+                    setBlog({...blog, content:data});
+                    setEditorState("Publish")
+                }else {
+                    return toast.error("Write Something in your blog to publish it.")
+                }
+            })
+            .catch ((err)=>{
+                console.log(err);
+            })
+        }
     }
   return (
     <>
     <div className='navbar'>
+        <Toaster/>
         <Link to ='/' className='flex-none w-10'>
         <img src ={logo}/>
         </Link>
-        <p className='max-md:hidden text-black line-clamp-1 w-full'>New Blog</p>
+        <p className='max-md:hidden text-black line-clamp-1 w-full'>{title.length ? title : "New Blog"}</p>
         <div className='flex gap-4 ml-auto'>
-            <button className='btn-dark py-2'>Publish</button>
+            <button className='btn-dark py-2'
+            onClick ={handlePublishEvent}
+            >Publish</button>
         <button className='btn-light py-2'>Save Draft</button>
         </div>
         </div>
@@ -35,8 +100,9 @@ let blogBannerRef = useRef();
                     <div className='relative aspect-video bg-white border-4 border-grey hover:opacity-80'>
                         <label>
                             <img
-                            src = {defaultBanner}
-                            ref={blogBannerRef}
+                            onError={handleError}
+                            src = {banner}
+                            // ref={blogBannerRef}
                             className='z-20'
                             />
                             <input 
@@ -48,7 +114,21 @@ let blogBannerRef = useRef();
                             />
                         </label>
                     </div>
+                    <textarea
+                    placeholder='Blog Title'
+                    className='text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading tight placeholder:opa city-40'
+                    onKeyDown={handleTitlekeyDown}
+                    onChange={handleTitleChange}
 
+                    >
+
+                    </textarea>
+<hr className='w-full opacity-10 my-5'/>
+
+<div id = 'textEditor' className=' font-gelasio'>
+
+
+</div>
                 </div>
             </section>
         </AnimationWrapper>
--- frontend/src/pages/editor.pages.jsx
+++ frontend/src/pages/editor.pages.jsx
@@ -1,18 +1,34 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState,createContext } from 'react'
 import { UserContext } from './../App';
-import { Navigate } from 'react-router-dom';
+import { Navigate, } from 'react-router-dom';
 import PublishForm from '../components/publish-form.component';
 import BlogEditor from '../components/blog-editor.component';
+const BlogStructure =
+{
+  title :'',
+  banner: '',
+  content:[],
+  tags:[],
+  des:'',
+  author:{personal_info:{ }}
+  
+}
+export const EditorContext = createContext ({
 
+})
 const Editor = () => {
-
+const [blog,setBlog] =useState(BlogStructure)
     const [editorState, setEditorState] =useState("editor");
-
+const [textEditor, setTextEditor] = useState({isReady:false});
     let {userAuth:{access_token }} = useContext(UserContext)
 
   return (
+    <EditorContext.Provider value ={{blog,setBlog,editorState,setEditorState,textEditor,setTextEditor}}>
+      {
     access_token === null ? <Navigate to ='/signin'/> :
-    editorState == "editor" ? <BlogEditor/> :<PublishForm/>
+    editorState == "editor" ? <BlogEditor/> :<PublishForm/> 
+    }
+    </EditorContext.Provider>
   )
 }
 
